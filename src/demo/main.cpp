6a2b58c77aea4fd69c4bfafe603ae14b8a033401
Dawid Borycki <dawid@borycki.com.pl>
2020-06-25 11:23:51 +0200

    Sine wave insert and select

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,5 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <influxdb_raw_db_utf8.h>
 #include <influxdb_simple_api.h>
 #include <influxdb_line.h>
@@ -8,45 +6,60 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <string>
 
 using namespace influxdb::api;
+
+double* generateSignal(int length) {
+    auto phaseStep = 2 * M_PI / length;
+
+    auto signal = new double[length];
+
+    for (int i = 0; i < length; i++) {
+        auto phase = 1.0 * i * phaseStep;        
+
+        signal[i] = sin(phase);
+    }
+
+    return signal;
+}
 
 int main(int argc, char* argv[])
 {
     try
     {
         const char* url = "http://localhost:8086";
-        influxdb::raw::db_utf8 db(url, "demo");
-        influxdb::api::simple_db api(url, "demo");
-        influxdb::async_api::simple_db async_api(url, "demo");
+        std::string dbName = "iotdb";
+        influxdb::raw::db_utf8 db(url, dbName);
+        influxdb::api::simple_db api(url, dbName);        
 
-        api.drop();
+        // Create database
         api.create();
+        
+        // Generate sine wave data
+        const int N = 512;
+        auto data = generateSignal(N);        
 
-        // {"results":[{"series":[{"columns":["name"],"name":"databases","values":[["_internal"],["mydb"]]}]}]}
-        std::cout << db.get("show databases") << std::endl;
+        // Add sine wave to the measurement
+        std::string measurementName = "sineWave";
+        std::string columneNameSignal = "signal";
+        std::string columneNameSignalReversed = "signalReversed";
 
-        async_api.insert(line("test", key_value_pairs(), key_value_pairs("value", 41)));
-        api.insert(line("test", key_value_pairs(), key_value_pairs("value", 42)));
+        for(int i = 0; i < N; i++) {            
+            api.insert(line
+                (measurementName, key_value_pairs(), key_value_pairs(columneNameSignal, data[i]))
+                (measurementName, key_value_pairs(), key_value_pairs(columneNameSignalReversed, -data[i])));
+        }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(101));
+        // Delete signal
+        delete data;
 
-        // {"results":[{"series":[{"columns":["time","value"],"name":"test","values":[["2016-10-28T22:11:22.8110348Z",42]]}]}]}
-        std::cout << db.get("select * from demo..test") << std::endl;
+        // Query and display data from the database            
+        auto query = "SELECT " + columneNameSignalReversed + " FROM " + dbName + ".." + measurementName;        
+        std::cout << std::endl << db.get(query) << std::endl;
 
-        // or if the async call passes through:
-        // {"results":[{"series":[{"name":"test","columns":["time","value"],
-        //             "values":[["2016-12-09T20:24:18.8239801Z",42],["2016-12-09T20:24:18.9026688Z",41]]}]}]}
-
-        api.drop();
-
-        // multiple lines formatted for one synchronous call:
-        // multiple,v1=1i
-        // multiple,v2=2i
-        std::cout << line
-            ("multiple", key_value_pairs("v1", 1), key_value_pairs())
-            ("multiple", key_value_pairs("v2", 2), key_value_pairs())
-        .get() << std::endl;
+        // Drop database 
+        api.drop();                                
     }
     catch (std::exception const& e)
     {
